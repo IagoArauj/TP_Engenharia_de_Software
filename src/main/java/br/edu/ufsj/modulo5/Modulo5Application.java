8eefb9f47a72fa1946d553c84cfff915d8dea14a
Iago Araújo <iagoaoliveira14@outlook.com>
2021-04-01 01:11:32 -0300

    Initial commit

---
+++ src/main/java/br/edu/ufsj/modulo5/Modulo5Application.java
@@ -0,0 +1,13 @@
+package br.edu.ufsj.modulo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo5Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufsj/modulo5/Modulo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufsj.modulo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
